8ae4376b1554b6bf124cabbb5acadc81e9684b7a
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-23 15:54:10 -0600

    updated filed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
-export const App = () => {
+import React from 'react';
+import Profile from 'components/Profile/profile';
+import Statistics from 'components/Statistics/statistics';
+import FriendList from 'components/FriendList/friends';
+import Transactions from 'components/TransactionHistory/transactionHistory';
 
+const App = () => {
+  return (
+    <div>
+      <Profile />
+      <Statistics />
+      <FriendList />
+      <Transactions />
+    </div>
+  );
 };
+
+export default App;
