3e894139a2bcc70a784ced74279664ad87048934
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-23 15:44:24 -0600

    added files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import Profile from 'components/Profile/profile';
+import Statistics from 'components/Statistics/statistics';
+import FriendList from 'components/FriendList/friends';
+import Transactions from 'components/TransactionHistory/transactionHistory';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Profile></Profile>
+    <Statistics></Statistics>
+    <FriendList></FriendList>
+    <Transactions></Transactions>
   </React.StrictMode>
 );
