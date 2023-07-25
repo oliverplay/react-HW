9d8d83eaa3afaa21404b5ab5cbf622ec1d22cb54
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-24 20:43:35 -0600

    corrected the file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App';
 import './index.css';
-import Profile from 'components/Profile/profile';
-import Statistics from 'components/Statistics/statistics';
-import FriendList from 'components/FriendList/friends';
-import Transactions from 'components/TransactionHistory/transactionHistory';
+// import Profile from 'components/Profile/profile';
+// import Statistics from 'components/Statistics/statistics';
+// import FriendList from 'components/FriendList/friends';
+// import Transactions from 'components/TransactionHistory/transactionHistory';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Profile></Profile>
+    {/* <Profile></Profile>
     <Statistics></Statistics>
     <FriendList></FriendList>
-    <Transactions></Transactions>
+    <Transactions></Transactions> */}
   </React.StrictMode>
 );
