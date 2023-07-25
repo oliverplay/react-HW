c577b8da11a9fcdcfecb726424c2f6bb4d52b2e6
Oleg <116948332+oliverplay@users.noreply.github.com>
2023-07-25 10:03:08 +0300

    Update transactionHistory.js

--- src/components/TransactionHistory/transactionHistory.js
+++ src/components/TransactionHistory/transactionHistory.js
@@ -2,60 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import transactions from './transactions.json';
 import {
-    TableSection,
-    TransactionTable,
-    HeaderElement,
-    TransactionData,
-    TransactionDataTwo,
-} from './TransactionHistory.styled'
+  TableSection,
+  TransactionTable,
+  HeaderElement,
+  TransactionDataTwo,
+} from './TransactionHistory.styled';
 
-const TransactionHistory = ({ 
-    
-    type, 
-    amount, 
-    currency }) => {
-    return (
+const TransactionHistory = ({ type, amount, currency }) => {
+  return (
     <tr>
-        <TransactionDataTwo>{type}</TransactionDataTwo>
-        <TransactionDataTwo>{amount}</TransactionDataTwo>
-        <TransactionDataTwo>{currency}</TransactionDataTwo>
+      <TransactionDataTwo>{type}</TransactionDataTwo>
+      <TransactionDataTwo>{amount}</TransactionDataTwo>
+      <TransactionDataTwo>{currency}</TransactionDataTwo>
     </tr>
-    )
-
+  );
 };
 
 TransactionHistory.propTypes = {
-    type: PropTypes.string.isRequired,
-    amount: PropTypes.string.isRequired,
-    currency: PropTypes.string.isRequired,
-  };
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
 
 const Transactions = () => {
-    return (
-        <TableSection>
-        <TransactionTable>
-            <thead>
-                <tr>
-                    <HeaderElement>Type</HeaderElement>
-                    <HeaderElement>Amount</HeaderElement>
-                    <HeaderElement>Currency</HeaderElement>
-                </tr>
-            </thead>
+  return (
+    <TableSection>
+      <TransactionTable>
+        <thead>
+          <tr>
+            <HeaderElement>Type</HeaderElement>
+            <HeaderElement>Amount</HeaderElement>
+            <HeaderElement>Currency</HeaderElement>
+          </tr>
+        </thead>
 
-            <tbody>
-            {transactions.map(transaction => (
-                <TransactionHistory
-                    key={transaction.id}
-                    type={transaction.type}
-                    amount={transaction.amount}
-                    currency={transaction.currency}
-                
-                ></TransactionHistory>
-            ))}
-            </tbody>
-        </TransactionTable></TableSection>
-    )
-}
+        <tbody>
+          {transactions.map(transaction => (
+            <TransactionHistory
+              key={transaction.id}
+              type={transaction.type}
+              amount={transaction.amount}
+              currency={transaction.currency}
+            ></TransactionHistory>
+          ))}
+        </tbody>
+      </TransactionTable>
+    </TableSection>
+  );
+};
 
-
-export default Transactions
+export default Transactions;
